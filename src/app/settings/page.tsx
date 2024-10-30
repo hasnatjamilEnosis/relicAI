0988048d7527608e4bf723a472f697314cba6040
hasnat.jamil <hasnatjamil2018@gmail.com>
2024-10-30 19:03:12 +0600

    Implement the settings page UI
    - add in tanstack form handler
    - add in required UI components from shadcn
    - add new component for updating the setting values
    - add the newly created update settings component to the settings page component

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,12 +1,30 @@
+"use client";
+
 import PageContent from "@/components/custom-ui/page-content";
 import PageHeader from "@/components/custom-ui/page-header";
+import UpdateSettings from "@/components/forms/settings/update-settings";
 
 export default function Settings() {
   return (
     <div>
       <PageHeader title="Settings" />
       <PageContent>
-        <p>Settings page content</p>
+        <UpdateSettings
+          jiraApiKey="12345678912345678921"
+          currentSelectedProjectId="1"
+          projectList={[
+            { value: "1", label: "Project 1" },
+            { value: "2", label: "Project 2" },
+            { value: "3", label: "Project 3" },
+            { value: "4", label: "Project 4" },
+          ]}
+          currentSelectedUserIds="1, 2"
+          userList={[
+            { value: "1", label: "John" },
+            { value: "2", label: "Mark" },
+            { value: "3", label: "Robert" },
+          ]}
+        />
       </PageContent>
     </div>
   );
---
+++ src/components/custom-ui/form-field-container.tsx
@@ -0,0 +1,7 @@
+export default function FieldContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="my-1.5">{children}</div>;
+}
